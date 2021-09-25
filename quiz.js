3caacb24ab6641712f57b234b5e5e6ba06d38111
adrienne_b <WorldofArts_95@yahoo.com>
2021-09-25 11:51:52 -0500

    Put questions and multiple choice down, linked css and linked javascript.

---
+++ quiz.js
@@ -0,0 +1,20 @@
+function check() {
+    var c+0;
+    var q1=document.quiz.question1.value;
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+
+    if(q1=="Pennsylvania, Connecticut, S.Carolina, Maryland, New Jersey, Rhode Island, Massachusetts, New Hampshire, Delaware, Georgia, N.Carolina, Virginia, and New York.") {c++}
+    if(q2=="A and C") {c++}
+    if(q3=="9/9/1776, 12/18/1865") {c++}
+    if(q4=="That anyone that were slaves were now free under this new bill.") {c++}
+    if(q5=="A slave that had kids for President Thomas Jefferson.") {c++}
+    if(q6=="Stock holders were selling their stocks at once because they were scared that the stocks were declining.") {c++}
+    if(q7=="10 years.") {c++}
+    if(q8=="The awareness of women liberation and equality which started in the 1960s.") {c++}
+    if(q9=="Because it represented freedom and strenght of America.") {c++}
+    if(q10=="2") {c++}
+ 
+}
