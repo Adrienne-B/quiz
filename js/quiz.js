88f98c0f0027d7ab0470f68c0c09b7a86f1b04c0
adrienne_b <WorldofArts_95@yahoo.com>
2022-05-16 16:04:54 -0500

    There needed to be a second closing curly bracket in css styles, js and index code was corrected to be the exact same, there needed to be === NOT == for js comparison, and I had to remove out display none that was in js.

--- js/quiz.js
+++ js/quiz.js
@@ -13,19 +13,21 @@ function check() {
     var q10=document.quiz.question10.value;
     var result=document.getElementById('result');
 
-    if(q1=="Pennsylvania, Connecticut, S.Carolina, Maryland, New Jersey, Rhode Island, Massachusetts, New Hampshire, Delaware, Georgia, N.Carolina, Virginia, and New York.") {c++}
-    if(q2=="A and C") {c++}
-    if(q3=="9/9/1776, 12/18/1865") {c++}
-    if(q4=="That anyone that were slaves were now free under this new bill.") {c++}
-    if(q5=="A slave that had kids for President Thomas Jefferson.") {c++}
-    if(q6=="Stock holders were selling their stocks at once because they were scared that the stocks were declining.") {c++}
-    if(q7=="10 years.") {c++}
-    if(q8=="The awareness of women liberation and equality which started in the 1960s.") {c++}
-    if(q9=="Because it represented freedom and strenght of America.") {c++}
-    if(q10=="2") {c++}
+    // Pennsylvania, Connecticut, S.Carolina, Maryland, New Jersey, Rhode Island, Massachusetts, New Hampshire, Delaware, Georgia, N.Carolina, Virginia, and New York
+    if(q1==="Pennsylvania, Connecticut, S.Carolina, Maryland, New Jersey, Rhode Island, Massachusetts, New Hampshire, Delaware, Georgia, N.Carolina, Virginia, and New York") {c++}
+    if(q2==="A and C") {c++}
+    if(q3==="9/9/1776, 12/18/1865") {c++}
+    if(q4==="That anyone that were slaves were now free under this new bill") {c++}
+    if(q5==="A slave that had kids for President Thomas Jefferson") {c++}
+    if(q6==="Stock holders were selling their stocks at once because they were scared that the stocks were declining") {c++}
+    if(q7==="10 years") {c++}
+    if(q8==="The awareness of women liberation and equality which started in the 1960s") {c++}
+    if(q9==="Because it represented freedom and strength of America") {c++}
+    if(q10==="2") {c++}
 
-    /*document.write("You are done, Congradulations!");*/
-
-    result.textContent='$(c)';
- 
+    if (c<=6) {
+        result.textContent=`Your result is ${c}.  It is below average. Please try again.`
+    } else {
+        result.textContent=`You did great! Your result is ${c} You know your American History!`
+    }
 }
